d83d31fd2566bb48de1ab88572ed47406cd95a84
Esh shatabdi <eshshatabdi@gmail.com>
2022-04-04 13:51:03 -0400

    delete item form home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
             <Link to='/dashboard'>DASHBOARD</Link>
             <Link to='/blogs'>BLOGS</Link>
             <Link to='/about'>ABOUT</Link>
-            {/* <Link to='/'>HOME</Link> */}
+
 
 
         </nav>
